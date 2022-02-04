e4d0ff2678b2992c598a7a469e1784114196afab
edinmukinovic <edin.mukinovic@imel.ba>
2022-02-04 09:20:02 +0100

    update

--- app.js
+++ app.js
@@ -119,67 +119,6 @@ expandMenu.addEventListener("mouseleave", (e) => {
 
 
 
-let items_array = [
-  { "name": "carrots", "id": 1, count: 1 },
-  { "name": "milk", "id": 2, count: 1 },
-  { "name": "cheese", "id": 3, count: 1 },
-  { "name": "pottato", "id": 4, count: 1 },
-  { "name": "eggs", "id": 5, count: 1 }
-];
-
-let cart = [];
-
-
-function appendNode(parent, element) {
-  parent.appendChild(element);
-};
-
-function getDiv(container) {
-  return document.getElementById(container);
-};
-
-function createNode(node) {
-  let element = document.createElement(node);
-  return element;
-};
-
-function displayItems(items, container) {
-  let items_container = getDiv(container);
-  items_container.innerHTML = '';
-
-  for (let i = 0; i < items.length; i++) {
-      let item = items[i];
-
-      let item_node = createNode("li");
-      item_node.setAttribute("id", item.id);
-
-      if (item.count > 0) {
-          item_node.innerHTML = `${item.name} 
-          <span id="badge">${item.count}</span>`;
-          appendNode(items_container, item_node);
-      }
-  }
-}
-
-displayItems(items_array, "items");
-
-function addOrRemoveItemsFromCart(action) {
-  let container = '';
-
-  if (action == "add") {
-      container = getDiv("items");
-
-      takeAction(container)
-  }
-  else if (action == "remove") {
-      container = getDiv("cart");
-
-      takeAction(container)
-  };
-}
-
-
-
 
 
 
